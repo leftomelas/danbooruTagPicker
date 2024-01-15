77950384633a91efa4fc38eb66ac6a593c9a7cdd
gegebo001 <gegebo001@gmail.com>
2024-01-15 18:50:25 +0900

    add ignore tags

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # danbooruTagPicker
-これはchromeの拡張です、開いたdanbooruからanimagine ver3.0の推奨する並び順でクリップボードにコピーします
-This is a chrome extension that copies from the opened danbooru to the clipboard in the order recommended by animagine ver 3.0
+これはchromeの拡張です、開いたdanbooruからanimagine ver3.0の推奨する並び順でクリップボードにコピーします  
+This is a chrome extension that copies from the opened danbooru to the clipboard in the order recommended by animagine ver 3.0  
+  
+## ver0.2  
+ちょっとだけ大規模にほぼ作り直し  
+前のバージョンでカスタマイズした人ごめんねー、もう一回カスタマイズできるよ！  
+タグの除去リストを2種類追加、学習はともかく生成に不要な"username", "logo", "watermark"などを簡単に除去できるようになった  
+  
+### カスタマイズは自分で書いて？  
+service-worker.jsの108行目あたりまでは読まなくてOK、そこから先でカスタマイズをやろう  
+120行目あたりのignoreTagsが完全一致のタグ除去リスト  
+135行目あたりのignoreTagsRegで部分一致のタグ除去リスト  
+166行目あたりのresultに追加したいタグを書く(lora可になった)  
+  
+ちなみに部分一致でbreastsを消して、タグ追加にflat chestを入れると全てのキャラが貧乳化するよ！  
+  
+## ver0.1  
+とりあえず作る  
--- service-worker.js
+++ service-worker.js
@@ -1,70 +1,185 @@
 function copyToClipbordTag() {
 
-  function filter_tag(tagString) {
-    result = ""
+
+  //danbooruよりデーターを読み取りタグを配列に入れて返す
+  function getTagFromDanbooru(tagString) {
+    result = []
     var elements = document.querySelectorAll(tagString);
 
     elements.forEach(element => {
       var tags = element.querySelectorAll("li[data-tag-name]");
+
       tags.forEach(tag => {
-        result += tag.dataset.tagName + ","
+        var tagstring = tag.dataset.tagName
+        result.push(tagstring)
       });
     });
     return result;
   };
 
-  // 正規表現を使って文字列の先頭に移動する関数
-  function moveCharacterToStart(inputString, targetCharacter) {
-    // 正規表現で対象の文字を検索
-    var regex = new RegExp(targetCharacter);
-    var match = inputString.match(regex);
-    // 対象の文字が見つかった場合、文字列の先頭に移動
-    if (match) {
-      return match[0] + "," + inputString.replace(regex, '').replace(",,", ",");
 
-    } else {
-      // 対象の文字が見つからない場合は元の文字列を返す
-      return inputString;
-    }
+  //tagsからignoreTagsに一致するタグを除去
+  //完全一致
+  function filterIgnoreTags(tags, ignoreTags) {
+
+    //スぺースをアンダーバーにする
+    ignoreTags = ignoreTags.reduce((p, c, i) => {
+      p.push(c.replace(" ", "_"))
+      return p
+    }, [])
+
+    result = tags.reduce((p, c, i) => {
+      if (ignoreTags.includes(c)) {
+        console.log("filterIgnoreTagsで除外(完全一致):" + c)
+      } else {
+        p.push(c)
+      }
+      return p
+    }, []);
+    return result;
   }
 
 
-  var tagstring = ""
-  tagstring += filter_tag('ul.character-tag-list');
- 
+  //tagsからignoreTagsに一致するタグを除去
+  //部分一致
+  function filterPartialIgnoreTags(tags, ignoreTags) {
+
+    //一部マッチさせたい文字列を格納した配列から正規表現のregexを作る
+    patternText = ignoreTags.reduce((p, c, i) => {
+      return p + "|" + c.replace(" ", "_")
+    }, "")
+    patternText = ".*(" + patternText.slice(1) + ").*"
+    reg = new RegExp(patternText, "i")
+
+    //実際にフィルターを掛ける
+    result = tags.reduce((p, c, i) => {
+      if (reg.test(c)) {
+        console.log("filterPartialIgnoreTagsで除外(正規表現):" + c)
+      } else {
+        p.push(c)
+      }
+      return p
+    }, []);
+    return result;
+  }
+
+  //アンダーバーをスペースに変換
+  function convertUnderbar2Space(tags) {
+    result = tags.reduce((p, c, i) => {
+      tag = c.replace(/_/g, " ");
+      p.push(tag)
+      return p
+    }, []);
+    return result;
+  }
+
+  // ()<>[]をエスケープ
+  function escapeChar(tags) {
+    result = tags.reduce((p, c, i) => {
+      tag = c.replace(/[\(\)\<\>\[\]]/g,
+        (match, p1, offset, string) => {
+          return "\\" + match;
+        }
+      );
+      p.push(tag)
+      return p
+    }, []);
+    return result;
+  }
+
+  // 1boy,1girlなどを分離する
+  function captureGirlBoy(tags) {
+
+    resultFront = []
+    resultBehind = []
+
+    reg = RegExp(/^([1-9]boy[s]*|[1-9]girl[s]*)$/, "i")
+    tags.forEach((tag) => {
+      if (reg.test(tag)) {
+        resultFront.push(tag)
+      } else {
+        resultBehind.push(tag)
+      }
+    })
+    return [resultFront, resultBehind]
+  }
+
+  //カスタマイズする人はここから先で
+  var tags = []
+  //タグ読み込み
+  tags = tags.concat(getTagFromDanbooru('ul.character-tag-list'));
   //作者タグは外します、使う人はコメントアウト外してね
-  //tagstring += filter_tag('ul.artist-tag-list');
-  tagstring += filter_tag('ul.copyright-tag-list');
-  tagstring += filter_tag('ul.general-tag-list');
+  //tags = tags.concat(getTagFromDanbooru('ul.artist-tag-list'));
+  tags = tags.concat(getTagFromDanbooru('ul.copyright-tag-list'));
+  tags = tags.concat(getTagFromDanbooru('ul.general-tag-list'));
 
-  //タグを追加していく作業はここで終わり
-  //最後にカンマがきてるはずなので削除する
-  tagstring = tagstring.slice(0, -1);
 
-  tagstring = moveCharacterToStart(tagstring, "[1-9]boy[s]*")
-  tagstring = moveCharacterToStart(tagstring, "[1-9]girl[s]*")
+  //除外タグ、完全一致
+  //かっこのエスケープ不要、アンダーバーありなしどっちでもＯＫ
+  //pubic hairなどを部分一致で使うとmale pubic hairなども消えるのでそれが嫌ならここに書くこと
+  var ignoreTags = [
+    // "monochrome", "greyscale",
+    // "thought_bubble", "speech_bubble", "comic", "sound effects", "heart", "heartbeat", "spoken heart",
+  ]
+  tags = filterIgnoreTags(tags, ignoreTags);
 
-  tagstring = tagstring.replace(/,,/g, ",");
-  tagstring = tagstring.replace(/_/g, " ");
-  tagstring = tagstring.replace(/\(/g, "\\\(");
-  tagstring = tagstring.replace(/\)/g, "\\\)");
-  
-  tagstring = tagstring.replace(/\[/g, "\\\[");
-  tagstring = tagstring.replace(/\]/g, "\\\]");
-  
-  tagstring = tagstring.replace(/\</g, "\\\<");
-  tagstring = tagstring.replace(/\>/g, "\\\>");
 
-  //不要な人はコメントアウトしてね？
-  tagstring += ",masterpiece,best quality"  
+  //除外タグ、正規表現を使った部分一致
+  //よくわからない人は英数字のみで書けば部分一致として機能する
+  //かっこのエスケープ不要、アンダーバーありなしどっちでもＯＫ
+  //
+  // （わかる人向け）例えばこれが
+  // ignoreTagsReg=["hoge","pubic hair","f.*ck"]
+  // こんな風に正規表現に変形する
+  //  /.*(hoge|pubic_hair|f.*ck).*/i
+  var ignoreTagsReg = [
+    "username", "logo", "watermark", "signature", "artist name", "copyright", "address", "text",
+    //"censor",
+    //"pubic hair",
+  ]
+  tags = filterPartialIgnoreTags(tags, ignoreTagsReg);
 
-  navigator.clipboard.writeText(tagstring)
+  //タグのアンダーバーをスペースに変換する
+  //したくないならコメントアウトで
+  tags = convertUnderbar2Space(tags)
+
+  //かっこ等をエスケープ
+  tags = escapeChar(tags)
+
+  var result = ""
+
+  //1girlなどの前に文字列を入れたい場合はここを使う
+  //result += "1haeteruko,"
+
+  //1girl等をつなげる
+  var [boyGirlTag, otherTags] = captureGirlBoy(tags)
+  result += boyGirlTag.join(",") + ","
+
+  //1girlなどの後に文字を入れたい場合はここを使う
+  //result += "himekishi chan,"
+
+  //他のタグをつなげる
+  result += otherTags.join(",") + ","
+
+  //一番最後に任意の文字を入れたい場合はここを使う
+  result += "explicit, nsfw, masterpiece,best quality"
+
+  //,が二つ続いたらいっこ削除
+  result = result.replace(",,", ",");
+
+  //最後に,が来てる場合は削除
+  if (result.endsWith(',')) {
+    result = result.slice(0, -1);
+  }
+
+  navigator.clipboard.writeText(result)
     .then(() => {
-      console.log('クリップボードにコピーしました:' + tagstring);
+      console.log('クリップボードにコピーしました:' + result);
     })
     .catch((error) => {
       console.error('クリップボードへのコピーに失敗しました: ', error);
     });
+
 }
 
 
@@ -76,4 +191,3 @@ chrome.action.onClicked.addListener((tab) => {
     });
   }
 });
-
